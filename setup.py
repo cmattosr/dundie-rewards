19b84e83e3a9d481c210b35751c583829c549759
cmattosr <cmrocha@gmail.com>
2024-05-02 23:45:41 -0300

    set up project initial organization and metadata: closes #8

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dundie",
+    version="0.1.0", # semantic versioning
+    description="Reward Point System for Dunder Mifflin",
+    author="Cesar Rocha",
+    packages=find_packages(),   
+)
