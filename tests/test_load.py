85d15b7b43e908b60c6abd15aab86ea7c16af679
cmattosr <cmrocha@gmail.com>
2024-05-23 20:14:54 -0300

    causing test error

--- tests/test_load.py
+++ tests/test_load.py
@@ -3,26 +3,10 @@ import pytest
 from dundie.core import load
 from constants import PEOPLE_FILE
 
-def setup_module():
-    """Setup module"""
-    print("roda antes dos testes desse modulo\n")
-    
-def teardown_module():
-    """Setup module"""
-    print("roda após os testes desse modulo\n")
-
-@pytest.fixture(scope="function",autouse=True)
-def create_new_file(tmpdir):
-    """Create a new file for testing"""
-    file_ = tmpdir.join("new_file.txt")
-    file_.write("isso é sujeira ...")
-    yield
-    file_.remove()
-
 
 @pytest.mark.unit
 @pytest.mark.high
-def test_load(request):
+def test_load_positive_has_2_people(request):
     """Test load function"""
     
     request.addfinalizer(lambda: print("\nTerminou"))
@@ -34,9 +18,11 @@ def test_load(request):
     
 @pytest.mark.unit
 @pytest.mark.high
-def test_load2():
+def test_load_positive_first_name_starts_with_j(request):
     """Test load function"""
+    
+    request.addfinalizer(lambda: print("\nTerminou"))
+    
     with open(f"arquivo_indesejado-{uuid.uuid4()}.txt", "w") as file_:
         file_.write("dados uteis somente para o teste")
-    assert len(load(PEOPLE_FILE)) == 2
     assert load(PEOPLE_FILE)[0][0] == 'J'
