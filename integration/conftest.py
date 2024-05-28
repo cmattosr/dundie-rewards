767f424e746d2989900e4e0fca078197fce685ab
cmattosr <81440986+cmattosr@users.noreply.github.com>
2024-05-27 23:50:09 -0300

    Linters (#14)
    * Adding finalizers, setup and teardown to tests
    
    * causing test error
    
    * add junit reports
    
    * add junit reports 2
    
    * add junit reports 4
    
    * add junit reports 5
    
    * add junit reports 6
    
    * add junit reports 7
    
    * add linter
    
    * add isort
    
    * fix linters

--- integration/conftest.py
+++ integration/conftest.py
@@ -5,5 +5,9 @@ medium: Medium priority
 low: Low priority
 """
 
+
 def pytest_configure(config):
-    map(lambda line: config.addinivalue_line("markers", line), MARKER.split("\n"))
+    map(
+        lambda line: config.addinivalue_line("markers", line),
+        MARKER.split("\n"),  # noqa
+    )  # noqa
